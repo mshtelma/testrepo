a4debde2cb1c5ad717662e8eca1b6924553c602c
Michael Shtelma <mshtelma@gmail.com>
2019-10-24 11:43:16 +0200

    xxx

---
+++ mmm.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %scala
+# MAGIC val a = 1+2
+# MAGIC a
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC val df = spark.parallelize(List(1,2,3,4))
+
+# COMMAND ----------
+
